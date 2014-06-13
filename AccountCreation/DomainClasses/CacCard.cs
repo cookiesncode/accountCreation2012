3d5501d54b16228f061dfcff544a97769ecc8616
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-06-13 09:17:32 -0600

    Refactored domain classes:
    - CurrentUser class is deleted
    - AdAccount class no longer dependant on EDIPI
    - Added new CacCard class to just handle the CAC info from the user

--- AccountCreation/DomainClasses/CacCard.cs
+++ AccountCreation/DomainClasses/CacCard.cs
@@ -13,26 +13,33 @@ namespace AccountCreation
 			get
 			{
 				HttpClientCertificate cacCard = HttpContext.Current.Request.ClientCertificate;
-				string[] subjectArray = cacCard.Subject.Split(',');
-				string subjectLine = cacCard.Subject.ToString();
-				int subjectLineLength = subjectLine.Length;
-				const int edipiLength = 10;
-				int cacIdentifierPosition = subjectLineLength - edipiLength;
-				string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
-				string[] arr = subjectArray[5].Split(' ');
-				string[] user = arr[1].Split('=');
-
-				StringBuilder sb = new StringBuilder();
-				foreach (string field in user)
+				if (cacCard.IsPresent)
 				{
-					sb.Append(field);
+					string[] subjectArray = cacCard.Subject.Split(',');
+					string subjectLine = cacCard.Subject.ToString();
+					int subjectLineLength = subjectLine.Length;
+					const int edipiLength = 10;
+					int cacIdentifierPosition = subjectLineLength - edipiLength;
+					string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
+					string[] arr = subjectArray[5].Split(' ');
+					string[] user = arr[1].Split('=');
+
+					StringBuilder sb = new StringBuilder();
+					foreach (string field in user)
+					{
+						sb.Append(field);
+					}
+					sb.Remove(0, 2);
+
+					string str1 = sb.ToString();
+					string[] fullName = str1.Split('.');
+
+					return fullName[0].ToString();
 				}
-				sb.Remove(0, 2);
-
-				string str1 = sb.ToString();
-				string[] fullName = str1.Split('.');
-
-				return fullName[0].ToString();
+				else
+				{
+					return null;
+				}
 			}
 		}
 		
@@ -41,26 +48,33 @@ namespace AccountCreation
 			get
 			{
 				HttpClientCertificate cacCard = HttpContext.Current.Request.ClientCertificate;
-				string[] subjectArray = cacCard.Subject.Split(',');
-				string subjectLine = cacCard.Subject.ToString();
-				int subjectLineLength = subjectLine.Length;
-				const int edipiLength = 10;
-				int cacIdentifierPosition = subjectLineLength - edipiLength;
-				string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
-				string[] arr = subjectArray[5].Split(' ');
-				string[] user = arr[1].Split('=');
-
-				StringBuilder sb = new StringBuilder();
-				foreach (string field in user)
+				if (cacCard.IsPresent)
 				{
-					sb.Append(field);
+					string[] subjectArray = cacCard.Subject.Split(',');
+					string subjectLine = cacCard.Subject.ToString();
+					int subjectLineLength = subjectLine.Length;
+					const int edipiLength = 10;
+					int cacIdentifierPosition = subjectLineLength - edipiLength;
+					string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
+					string[] arr = subjectArray[5].Split(' ');
+					string[] user = arr[1].Split('=');
+
+					StringBuilder sb = new StringBuilder();
+					foreach (string field in user)
+					{
+						sb.Append(field);
+					}
+					sb.Remove(0, 2);
+
+					string str1 = sb.ToString();
+					string[] fullName = str1.Split('.');
+
+					return fullName[1].ToString();
 				}
-				sb.Remove(0, 2);
-
-				string str1 = sb.ToString();
-				string[] fullName = str1.Split('.');
-
-				return fullName[1].ToString();
+				else
+				{
+					return null;
+				}
 			}
 		}
 		
@@ -69,27 +83,60 @@ namespace AccountCreation
 			get
 			{
 				HttpClientCertificate cacCard = HttpContext.Current.Request.ClientCertificate;
-				string[] subjectArray = cacCard.Subject.Split(',');
-				string subjectLine = cacCard.Subject.ToString();
-				int subjectLineLength = subjectLine.Length;
-				const int edipiLength = 10;
-				int cacIdentifierPosition = subjectLineLength - edipiLength;
-				string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
-				string[] arr = subjectArray[5].Split(' ');
-				string[] user = arr[1].Split('=');
-
-				StringBuilder sb = new StringBuilder();
-				foreach (string field in user)
+				if (cacCard.IsPresent)
 				{
-					sb.Append(field);
+					string[] subjectArray = cacCard.Subject.Split(',');
+					string subjectLine = cacCard.Subject.ToString();
+					int subjectLineLength = subjectLine.Length;
+					const int edipiLength = 10;
+					int cacIdentifierPosition = subjectLineLength - edipiLength;
+					string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
+					string[] arr = subjectArray[5].Split(' ');
+					string[] user = arr[1].Split('=');
+
+					StringBuilder sb = new StringBuilder();
+					foreach (string field in user)
+					{
+						sb.Append(field);
+					}
+					sb.Remove(0, 2);
+
+					string str1 = sb.ToString();
+					string[] fullName = str1.Split('.');
+
+					return cacIdentifier;
 				}
-				sb.Remove(0, 2);
-
-				string str1 = sb.ToString();
-				string[] fullName = str1.Split('.');
-
-				return cacIdentifier;
+				else
+				{
+					return null;
+				}
 			}
 		}
 	}
 }
+
+			//string[] subjectArray = cacCard.Subject.Split(',');
+			//string subjectLine = cacCard.Subject.ToString();
+			//int subjectLineLength = subjectLine.Length;
+			//const int edipiLength = 10;
+			//int cacIdentifierPosition = subjectLineLength - edipiLength;
+			//string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
+			//string[] arr = subjectArray[5].Split(' ');
+			//string[] user = arr[1].Split('=');
+
+			//StringBuilder sb = new StringBuilder();
+			//foreach (string field in user)
+			//{
+			//	sb.Append(field);
+			//}
+			//sb.Remove(0, 2);
+
+			//string str1 = sb.ToString();
+			//string[] fullName = str1.Split('.');
+
+			//LastName = fullName[0].ToString();
+			//FirstName = fullName[1].ToString();
+			//// TODO: fix middleinitial
+			//MiddleInitial = fullName[2].ToString();
+			//Edipi = cacIdentifier;
+			//AccountInfo = new ADAccount(cacIdentifier);
