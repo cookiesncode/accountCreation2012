c5dc9620283c4b2251ec813bc8e7aad8c733a432
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-07-24 15:00:23 -0600

    Added new MiddleInitial property to the  CacCard class.

--- AccountCreation/DomainClasses/CacCard.cs
+++ AccountCreation/DomainClasses/CacCard.cs
@@ -6,7 +6,10 @@ using System.Web;
 
 namespace AccountCreation
 {
-	public static class CacCard
+    // This Class is responsible for parsing the information found in the user's CAC card and extracting the desired values.
+    // This is an example of the string that is found in the CAC card for a user WITH a middle name: C=US, O=U.S. Government, OU=DoD, OU=PKI, OU=CONTRACTOR, CN=LASTNAME.FIRSTNAME.MIDDLENAME.1265020972
+    // This is an example of the string that is found in the CAC card for a user WITHOUT a middle name: C=US, O=U.S. Government, OU=DoD, OU=PKI, OU=CONTRACTOR, CN=LASTNAME.FIRSTNAME.1265020972
+    public static class CacCard
 	{
 		public static string LastName
 		{
@@ -112,31 +115,52 @@ namespace AccountCreation
 				}
 			}
 		}
-	}
+       
+        public static string MiddleInitial
+        {
+            get
+            {
+                HttpClientCertificate cacCard = HttpContext.Current.Request.ClientCertificate;
+                if (cacCard.IsPresent)
+                {
+                    string[] subjectArray = cacCard.Subject.Split(',');
+                    string subjectLine = cacCard.Subject.ToString();
+                    int subjectLineLength = subjectLine.Length;
+                    const int edipiLength = 10;
+                    int cacIdentifierPosition = subjectLineLength - edipiLength;
+                    string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
+                    string[] arr = subjectArray[5].Split(' ');
+                    string[] user = arr[1].Split('=');
+
+                    StringBuilder sb = new StringBuilder();
+                    foreach (string field in user)
+                    {
+                        sb.Append(field);
+                    }
+                    sb.Remove(0, 2);
+
+                    string str1 = sb.ToString();
+                    string[] fullName = str1.Split('.');
+                    string middleName = fullName[2].ToString();
+                    string middleInitial = middleName[0].ToString();
+                    // The following statements checks whether the value found in the middleInitial variable is a integer or a letter.
+                    // If it is an integer then an empty string is returned.
+                    int valueReturned;
+                    bool isInteger = int.TryParse(middleInitial, out valueReturned);              
+                    if (isInteger)
+                    {
+                        return "";
+                    }
+                    else
+                    {
+                        return middleInitial;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
 }
-
-			//string[] subjectArray = cacCard.Subject.Split(',');
-			//string subjectLine = cacCard.Subject.ToString();
-			//int subjectLineLength = subjectLine.Length;
-			//const int edipiLength = 10;
-			//int cacIdentifierPosition = subjectLineLength - edipiLength;
-			//string cacIdentifier = subjectLine.Substring(cacIdentifierPosition, edipiLength);
-			//string[] arr = subjectArray[5].Split(' ');
-			//string[] user = arr[1].Split('=');
-
-			//StringBuilder sb = new StringBuilder();
-			//foreach (string field in user)
-			//{
-			//	sb.Append(field);
-			//}
-			//sb.Remove(0, 2);
-
-			//string str1 = sb.ToString();
-			//string[] fullName = str1.Split('.');
-
-			//LastName = fullName[0].ToString();
-			//FirstName = fullName[1].ToString();
-			//// TODO: fix middleinitial
-			//MiddleInitial = fullName[2].ToString();
-			//Edipi = cacIdentifier;
-			//AccountInfo = new ADAccount(cacIdentifier);
