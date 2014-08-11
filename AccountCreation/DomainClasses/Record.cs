2f596d0daa5c1fddd8c54bf3823c89b691b42dd5
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-08-11 13:43:56 -0600

    Added new Record class which queries database for existing records

---
+++ AccountCreation/DomainClasses/Record.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountCreation
+{
+    public static class Record
+    {
+        public static AccountRequest QueryRecords(string edipi, string accountType, string requestType)
+        {
+            var context = new CarsonAccountEntities();
+            var query = from request in context.AccountRequests
+                        where request.Edipi == edipi
+                            && request.AccountType == accountType
+                            && request.RequestType == requestType
+                        select request;
+            return query.First();
+        }
+    }
+}
