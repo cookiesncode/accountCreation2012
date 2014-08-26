2de69670a2f51763b20e2d624e2f45e44b197669
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-08-26 16:42:29 -0600

    Email.cs cleanup: Updated variable names and email message verbiage

--- AccountCreation/DomainClasses/Email.cs
+++ AccountCreation/DomainClasses/Email.cs
@@ -8,7 +8,7 @@ namespace AccountCreation
 {
     public class Email
     {
-        public static void SendEmail(string toAddress, string message, string signature, string searchUrl, string accountType)
+        public static void SendEmail(string toAddress, string message, string signature, string appLink, string accountType)
         {
             using (var smtp = new SmtpClient("155.214.100.30"))
             {
@@ -21,8 +21,9 @@ namespace AccountCreation
                 mail.Subject = "Fort Carson Accounts: account verification requested; DO NOT REPLY";
                 mail.Body += "This is an automated message. Please DO NOT REPLY to this email address" + doubleLineBreak + doubleLineBreak;
                 mail.Body += "This email is to inform you that a " + accountType +  " account request has been submitted by:" + lineBreak + signature + doubleLineBreak;
-                mail.Body += message + lineBreak;
-                mail.Body += searchUrl;
+                mail.Body += message + doubleLineBreak;
+                mail.Body += "Link to the account request:" + lineBreak;
+                mail.Body += appLink;
                 mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
                 smtp.Send(mail);
             }
