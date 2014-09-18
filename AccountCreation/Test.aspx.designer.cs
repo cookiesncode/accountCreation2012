2f9a60fbd2cdea588126c8bd626e239c53bbffbc
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-09-18 11:10:48 -0600

    Test page: took out old test code

--- AccountCreation/Test.aspx.designer.cs
+++ AccountCreation/Test.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace AccountCreation {
     public partial class Test {
         
         /// <summary>
-        /// _entity control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label _entity;
-        
-        /// <summary>
         /// _cacTest control.
         /// </summary>
         /// <remarks>
@@ -29,15 +20,6 @@ namespace AccountCreation {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label _cacTest;
-        
-        /// <summary>
-        /// _cacCert control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label _cacCert;
         
         /// <summary>
         /// _cacMiddleInitial control.
