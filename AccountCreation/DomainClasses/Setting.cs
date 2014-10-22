0ad9f74ba9aeaedb11702bc32c3402061e270011
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-10-22 09:35:02 -0600

    Updated the Settings class: Some properties now query the new Settings model instead of the code being hard coded in the file directly

--- AccountCreation/DomainClasses/Setting.cs
+++ AccountCreation/DomainClasses/Setting.cs
@@ -49,300 +49,60 @@ namespace AccountCreation
 				};
 			}
 		}
+ 
+        public static List<string> InstallationList
+        {
+            get
+            {
+                var context = new SettingEntities();
+                var query = from record in context.Installations
+                            select record.InstallationName;
+                return query.ToList();
+            }
+        }
 
-		public static string[] Installation
-		{
-			get
-			{
-				return new string[]
-				{
-					"Fort Carson",
-					"Pueblo Chemical Depot"
-				};
-			}
-		}
+        public static List<string> UnitList
+        {
+            get
+            {
+                var context = new SettingEntities();
+                var query = from record in context.UnitOrgs
+                            select record.UnitName;
+                return query.ToList();
+            }
+        }
 
-		public static string[] Persona
-		{
-			get
-			{
-				return new string[]
-				{
-					"CIV",
-					"CTR",
-					"MIL",
-					"FM",
-					"FN",
-					"LN",
-					"NAF",
-					"NFG",
-					"NGO",
-					"VOL"
-				};
-			}
-		}
+        public static List<string> BranchList
+        {
+            get
+            {
+                var context = new SettingEntities();
+                var query = from record in context.Branches
+                            select record.BranchName;
+                return query.ToList();
+            }
+        }
 
-		public static string[] Branch
-		{
-			get
-			{
-				return new string[]
-				{
-					"Air Force",
-					"Army",
-					"Coast Guard",
-					"Marine Corps",
-					"Navy"
-				};
-			}
-		}
+        public static List<string> PersonaList
+        {
+            get
+            {
+                var context = new SettingEntities();
+                var query = from record in context.Personas
+                            select record.PersonaName;
+                return query.ToList();
+            }
+        }
 
-		public static string[] OrgUnit
-		{
-			get
-			{
-				return new string[]
-				{
-                    "AFSBN",
-                    "DOL",
-                    "MICC",
-                    "PCD",
-                    "TMDE",
-                    "BFT",
-                    "CPAC",
-                    "DMPO",
-                    "WCAP",
-                    "1-25ATK",
-                    "4EN",
-                    "4ID",
-                    "4ID 1SBCT 1-38IN",
-                    "4ID 1SBCT 2-12FA",
-                    "4ID 1SBCT 2-1CAV",
-                    "4ID 1SBCT 2-23IN",
-                    "4ID 1SBCT 299EN",
-                    "4ID 1SBCT 4-9IN",
-                    "4ID 1SBCT 4BSB",
-                    "4ID 1SBCT HHC",
-                    "4ID 2BCT",
-                    "4ID 2BCT 1-10CAV",
-                    "4ID 2BCT 1-67AR",
-                    "4ID 2BCT 2-8IN",
-                    "4ID 2BCT 2STB",
-                    "4ID 2BCT 3-16FA",
-                    "4ID 2BCT 204BSB",
-                    "4ID 2BCT HQ",
-                    "4ID 3BCT",
-                    "4ID 3BCT 1-8IN",
-                    "4ID 3BCT 1-66AR",
-                    "4ID 3BCT 1-68AR",
-                    "4ID 3BCT 3-29FA",
-                    "4ID 3BCT 3STB",
-                    "4ID 3BCT 4-10CAV",
-                    "4ID 3BCT 64BSB",
-                    "4ID 3BCT HQ",
-                    "4ID 4BCT",
-                    "4ID 4BCT 1-12IN",
-                    "4ID 4BCT 2-12IN",
-                    "4ID 4BCT 2-77FA",
-                    "4ID 4BCT 3-61CAV",
-                    "4ID 4BCT 4STB",
-                    "4ID 4BCT 704BSB",
-                    "4ID 4BCT HQ",
-                    "4ID 4CAB 1-4ARB",
-                    "4ID 4CAB 2-4GSAB",
-                    "4ID 4CAB 3-4AHB",
-                    "4ID 4CAB 4-4ARB",
-                    "4ID 4CAB 404ASB",
-                    "4ID 4CAB HHC",
-                    "4ID DSTB",
-                    "4ID HQ",
-                    "4ID-CMD-GRP",
-                    "10CSH",
-                    "43SB",
-                    "43SB 43BSTB",
-                    "43SB 68CSB",
-                    "52EN",
-                    "71EOD",
-                    "759MP",
-                    "DRM",
-                    "MSE",
-                    "OSJA",
-                    "iPERMS",
-                    "1MOB",
-                    "13ASOS",
-                    "ACS",
-                    "ARC",
-                    "DES",
-                    "DET1-3WS",
-                    "DFMWR",
-                    "DHR",
-                    "DPTMS",
-                    "DPW",
-                    "EEO",
-                    "GRM",
-                    "GSD",
-                    "Lenel",
-                    "PAO",
-                    "Safety",
-                    "USAG",
-                    "USARE-DISA DET 3",
-                    "704MI",
-                    "902MI",
-                    "MEDDAC",
-                    "NEC",
-                    "JTAGS",
-                    "48MP",
-                    "ROTC",
-                    "10SFG",
-                    "DCS"				
-                };
-			}
-		}
-
-		public static string[] Rank
-		{
-			get
-			{
-				return new string[] 
-				{
-					"Mr",
-                    "Mrs",
-                    "Ms",
-					"PVT",
-					"PV2",
-					"PFC",
-					"SPC",
-					"CPL",
-					"SGT",
-					"SSG",
-					"SFC",
-					"MSG",
-					"1SG",
-					"SGM",
-					"CSM",
-					"SMA",
-					"WO1",
-					"CW2",
-					"CW3",
-					"CW4",
-					"CW5",
-					"2LT",
-					"1LT",
-					"CPT",
-					"MAJ",
-					"LTC",
-					"COL",
-					"BG",
-					"MG",
-					"LTG",
-					"GEN",
-					"AF-AB",
-					"AF-Amn",
-					"AF-A1C",
-					"AF-SrA",
-					"AF-SSgt",
-					"AF-TSgt",
-					"AF-MSgt",
-					"AF-1SG",
-					"AF-SMSgt",
-					"AF-1SG",
-					"AF-CMSgt",
-					"AF-1SG",
-					"AF-CCM",
-					"AF-CMSAF",
-					"AF-2d Lt",
-					"AF-1st Lt",
-					"AF-Capt",
-					"AF-Maj",
-					"AF-Lt Col",
-					"AF-Col",
-					"AF-Brig Gen",
-					"AF-Maj Gen",
-					"AF-Lt Gen",
-					"AF-Gen",
-					"AF-GAF",
-					"CG-SR",
-					"CG-SA",
-					"CG-FA",
-					"CG-AA",
-					"CG-SN",
-					"CG-FN",
-					"CG-AN",
-					"CG-PO3",
-					"CG-PO2",
-					"CG-PO1",
-					"CG-CPO",
-					"CG-SCPO",
-					"CG-MCPO",
-					"CG-CMCPO",
-					"CG-CWO2",
-					"CG-CWO3",
-					"CG-CWO4",
-					"CG-ENS",
-					"CG-LTJG",
-					"CG-LT",
-					"CG-LCDR",
-					"CG-CDR",
-					"CG-CAPT",
-					"CG-RDML",
-					"CG-RADM",
-					"CG-VADM",
-					"CG-ADM",
-					"N-SR",
-					"N-SA",
-					"N-SN",
-					"N-PO3",
-					"N-PO2",
-					"N-PO1",
-					"N-CPO",
-					"N-SCPO",
-					"N-MCPO",
-					"N-CMCPO",
-					"N-WO1",
-					"N-CWO2",
-					"N-CWO3",
-					"N-CWO4",
-					"N-CWO5",
-					"N-ENS",
-					"N-LTJG",
-					"N-LT",
-					"N-LCDR",
-					"N-CDR",
-					"N-CAPT",
-					"N-RDML",
-					"N-RADM",
-					"N-VADM",
-					"N-ADM",
-					"Mar-Pvt",
-					"Mar-PFC",
-					"Mar-LCpl",
-					"Mar-Cpl",
-					"Mar-Sgt",
-					"Mar-SSgt",
-					"Mar-GySgt",
-					"Mar-MSgt",
-					"Mar-1stSgt",
-					"Mar-MGySgt",
-					"Mar-SgtMaj",
-					"Mar-SgtMajMarCor",
-					"Mar-W1",
-					"Mar-W2",
-					"Mar-W3",
-					"Mar-W4",
-					"Mar-W5",
-					"Mar-2ndLt",
-					"Mar-1stLt",
-					"Mar-Capt",
-					"Mar-Maj",
-					"Mar-LtCol",
-					"Mar-Col",
-					"Mar-Bgen",
-					"Mar-MajGen",
-					"Mar-LtGen",
-					"Mar-Gen",
-					"Cadet"
-				};
-			}
-		}
+        public static List<string> RankList
+        {
+            get
+            {
+                var context = new SettingEntities();
+                var query = from record in context.Ranks
+                            select record.RankName;
+                return query.ToList();
+            }
+        }
 	}
 }
